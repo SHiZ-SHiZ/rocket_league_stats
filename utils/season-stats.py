ce46bc438419f78433111d5526212817e7ab165d
Ryan Woebkenberg <rzwoebkenberg@gmail.com>
2022-08-27 18:15:24 -0400

    wip

--- utils/season-stats.py
+++ utils/season-stats.py
@@ -1,5 +1,7 @@
 import os, re, glob, sys
 
+#TIMESTAMP -> 0   Game mode -> 1   Team -> 2   Player -> 3   Score -> 4   Goals -> 5   Assists -> 6   Saves -> 7   Shots -> 8   Damage -> 9   MVP -> 10   Team Score -> 11   Win -> 12   MMR -> 13   
+
 dir = os.getcwd()
 print(dir)
 path = 'D:\\rocket_league_statistics\\' + str((sys.argv)[1])
@@ -9,5 +11,5 @@ print(dir)
 i = 1
 for file in glob.glob("*.csv"):
     f = open(file, "r")
-    print(file)
-    print(repr(f.read()))
+    stats_map = (repr(f.read()).split('\\n', 1)[1]).split('\\n')
+    print(stats_map)
